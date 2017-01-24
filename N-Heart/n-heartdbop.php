30480c8d885bc94e113b21a1060430580421d8e6
Mobility Team <cviacmobility@gmail.com>
2017-01-24 15:08:26 +0530

    Update n-heartdbop.php

--- N-Heart/n-heartdbop.php
+++ N-Heart/n-heartdbop.php
@@ -346,7 +346,7 @@ function validation($data)
 		{
 		$db = connect_db();
 		$result=array();
-		$sql = "select to_mobile,name,email_id from invitation where mobile='$mobile'";
+		$sql = "select to_mobile,name,email_id from invitation where to_mobile='$mobile'";
 		$exe = $db->query($sql);
 		$db = null;
 		$dat = $exe->fetch_all(MYSQLI_ASSOC);
