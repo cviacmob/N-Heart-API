7d868eca4e0b47ba55b3547f7d035d1709d134f4
cviacmob <cviacmobility@gmail.com>
2017-01-25 11:22:58 +0530

    mysqldb config

--- N-Heart/mysql.php
+++ N-Heart/mysql.php
@@ -2,10 +2,10 @@
 
 function connect_db()
 	{
-	$server = 'localhost'; // this may be an ip address instead
-	$user = 'root';
-	$pass = '';
-	$database = 'n-heart'; // name of your database
+	$server = 'nheart.cviac.com'; // this may be an ip address instead
+	$user = 'aramesh';
+	$pass = 'cvi@c2016';
+	$database = 'nheartdb'; // name of your database
 	$connection = new mysqli($server, $user, $pass, $database);
 	return $connection;
 	}
